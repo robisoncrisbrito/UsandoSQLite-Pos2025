5626042771be581dc351b2410034d6739bee33d7
robisoncrisbrito <robison.brito@gmail.com>
2025-04-05 10:52:04 -0300

    programa completo :)

--- app/src/main/java/br/edu/utfpr/usandosqlite_pos2025/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2025/MainActivity.kt
@@ -88,11 +88,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun btPesquisarOnClick() {
+        val registros = banco.query( "cadastro", null, "_id=?", arrayOf(binding.etCod.text.toString() ), null, null, null )
+
+        if ( registros.moveToNext() ) {
+            binding.etNome.setText( registros.getString( NOME ) )
+            binding.etTelefone.setText( registros.getString( TELEFONE ) )
+        } else {
+            Toast.makeText( this, "Registro não encontrado", Toast.LENGTH_LONG ).show()
+        }
+
+
 
     }
 
     fun btListarOnClick() {
 
+        val registros = banco.query( "cadastro", null, null, null, null, null, null )
+
+        val saida = StringBuilder()
+
+        while ( registros.moveToNext() ) {
+            saida.append( registros.getString( NOME ) )
+            saida.append( " - " )
+            saida.append( registros.getString( TELEFONE ) )
+            saida.append( "\n" )
+        }
+
+        Toast.makeText( this, saida.toString(), Toast.LENGTH_LONG ).show()
+
     }
 
-}
+    companion object {
+        private const val ID = 0
+        private const val NOME = 1
+        private const val TELEFONE = 2
+    }
+
+} //fim da Activity
+
