d2218721cdfaf71dcea8299052b9d126a23c6059
robisoncrisbrito <robison.brito@gmail.com>
2025-04-05 09:46:02 -0300

    com compoenntes visuais na activity

--- app/src/main/java/br/edu/utfpr/usandosqlite_pos2025/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2025/MainActivity.kt
@@ -5,11 +5,60 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.utfpr.usandosqlite_pos2025.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println( "onCreate() executado" )
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView( binding.main )
+
+        setButtonsListeneres()
     }
+
+    fun setButtonsListeneres() {
+        binding.btIncluir.setOnClickListener {
+            btIncluirOnClick()
+        }
+
+        binding.btAlterar.setOnClickListener {
+            btAlterarOnClick()
+        }
+
+        binding.btExcluir.setOnClickListener {
+            btExcluirOnClick()
+        }
+
+        binding.btPesquisar.setOnClickListener {
+            btPesquisarOnClick()
+        }
+
+        binding.btListar.setOnClickListener {
+            btListarOnClick()
+        }
+    }
+
+    fun btIncluirOnClick() {
+
+    }
+
+    fun btAlterarOnClick() {
+
+    }
+
+    fun btExcluirOnClick() {
+
+    }
+
+    fun btPesquisarOnClick() {
+
+    }
+
+    fun btListarOnClick() {
+
+    }
+
 }
