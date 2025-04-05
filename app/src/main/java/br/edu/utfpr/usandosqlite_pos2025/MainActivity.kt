feb659428482fbaf493480466cff1895286a7738
robisoncrisbrito <robison.brito@gmail.com>
2025-04-05 09:56:20 -0300

    com criacao do banco

--- app/src/main/java/br/edu/utfpr/usandosqlite_pos2025/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2025/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.edu.utfpr.usandosqlite_pos2025
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,12 +12,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+    private lateinit var banco : SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate( layoutInflater )
         setContentView( binding.main )
 
         setButtonsListeneres()
+
+        banco = SQLiteDatabase.openOrCreateDatabase(
+            this.getDatabasePath( "dbfile.sqlite" ),
+            null
+        )
+
+        banco.execSQL( "CREATE TABLE IF NOT EXISTS cadastro ( " +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nome TEXT, telefone TEXT )" )
     }
 
     fun setButtonsListeneres() {
